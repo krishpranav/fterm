c6785ffca6e508caf64896017176f8e36d93bc27
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-13 22:35:40 +0530

    lib: routes[base router]

---
+++ lib/routes.dart
@@ -0,0 +1,18 @@
+/**
+ * @file routes.dart
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-13
+ * 
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ * 
+ */
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final routerKey = GlobalKey<NavigatorState>();
+final router = GoRouter(navigatorKey: routerKey, routes: [
+
+  ]
+);
