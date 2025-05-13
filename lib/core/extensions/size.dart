7254ed9073d61ce8be0a46c46a491608226d0617
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-13 22:38:40 +0530

    core: extensions[size]

--- lib/core/extensions/size.dart
+++ lib/core/extensions/size.dart
@@ -13,5 +13,8 @@ import 'package:flutter/material.dart';
 extension SizeSerializer on Size {
   Map<String, dynamic> toJson() => {'width': width, 'height': height};
 
-  static Size fromJson() => Size();
+  static Size fromJson(Map<String, dynamic> json) => Size(
+    json['width'] as double? ?? double.infinity,
+    json['height'] as double,
+  );
 }
